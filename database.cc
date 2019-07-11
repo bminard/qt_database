9cc03d6d088b02c888e0e308feb793318bf543a8
Brian Minard <bminard@flatfoot.ca>
2019-07-10 20:01:29 -0400

    Incorporate an example and update CMake to build
    The example comes from a portion of code provided by Davide Coppola. A
    URL is provided in the source.

--- database.cc
+++ database.cc
@@ -1,3 +1,25 @@
+// Example from Davide Coppola. Article at http://blog.davidecoppola.com/2016/11/howto-embed-database-in-application-with-sqlite-and-qt/.
+// See https://github.com/vivaladav/BitsOfBytes/blob/master/howto-embed-database-in-application-with-sqlite-and-qt/QtSqlExample/MainWindow.cpp
+
+#include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlDriver>
+#include <QSqlError>
+#include <QSqlQuery>
+
 int main()
 {
+    const QString DRIVER("QSQLITE");
+    if(QSqlDatabase::isDriverAvailable(DRIVER)) {
+        QSqlDatabase db = QSqlDatabase::addDatabase(DRIVER);
+
+        db.setDatabaseName(":memory:");
+
+        if(!db.open()) {
+            qWarning() << "MainWindow::DatabaseConnect - ERROR: " << db.lastError().text();
+        }
+    }
+    else {
+        qWarning() << "MainWindow::DatabaseConnect - ERROR: no driver " << DRIVER << " available";
+    }
 }
